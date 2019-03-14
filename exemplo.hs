e528301ed0d118eff8533f78b4867b276adf8f0c
Geovana Silveira <geovanasilvadasilveira1@gmail.com>
2019-03-14 01:38:37 -0300

    Update exemplo.hs

--- exemplo.hs
+++ exemplo.hs
@@ -1,8 +1,8 @@
---      exemplo.hs--    comentario--
-idade :: Int  -- Um valor inteiro constante
-idade = 17
 -- exemplo.hs
 --
+idade :: Int  -- Um valor inteiro constante
+idade = 17
+
 maiorDeIdade :: Bool  -- Usa a definicao de
 maiorDeIdade = (idade>=18) --idade 
 
@@ -12,8 +12,9 @@ maiorDeIdade2 n
     | otherwise = False
 
 quadrado :: Int -> Int   -- funcao que eleva num
-quadrado x = x*x       -- ao quadradomini :: Int -> Int -> Int --funcao que mostra
+quadrado x = x*x       -- ao quadrado
 
+mini :: Int -> Int -> Int --funcao que mostra
 mini a b                  --o menor entre
   | a <= b    = a       -- dois valores
   | otherwise = b
@@ -38,22 +39,14 @@ fib n
  | (n==1) || (n==2) = 1
  | otherwise = fib(n-1) + fib(n-2)
 
---isPrime :: Int -> Bool
---isPrime n 
---  | (n%2 == 0) = True
---  | otherwise = False
+isPrime :: Int -> Bool
+isPrime 1 = False
+isPrime 2 = True
+isPrime n | (mod n 2 == 0)  = False
+    | otherwise = True
 
 maiorDeTres :: Int -> Int -> Int -> Int
 maiorDeTres x y z
   | (x > y) && (x > z) = x
   | (y > x) && (y > z) = y
   |otherwise = z
-
-insere :: Int -> [Int]
-insere a [] = [a]
-insere a (b:x)
-  | a <= b = a:(b:x)
-  | otherwise = b : insere a x 
-
-listaOrd :: Int -> Int -> Int -> [Int]
-listaOrd (a:x) = insere a (listaOrd x)
