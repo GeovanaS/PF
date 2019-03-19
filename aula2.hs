3fcdac027cd2b58c450c75678ce33846643d068c
Geovana Silveira <geovanasilvadasilveira1@gmail.com>
2019-03-19 18:38:13 -0300

    Add files via upload

---
+++ aula2.hs
@@ -0,0 +1,63 @@
+vendas :: Int -> Int
+vendas 0 = 11
+vendas 1 = 35
+vendas 2 = 20
+vendas 3 = 0
+vendas _ = 55
+
+vendaTotal :: Int -> Int
+vendaTotal 0 = vendas 0 
+vendaTotal n = vendas n + vendaTotal(n-1)
+
+inicio :: String
+inicio = "Semana   Vendas \n"  
+
+geraVendas :: Int -> String
+geraVendas n
+   | (n==0) = "Semana " ++ show(n) ++" "++ show(vendas 0)
+   | otherwise = "Semana " ++ show(n+1) ++" "++ show(geraVendas(n-1))
+
+tabela :: Int -> String
+tabela n = inicio ++ geraVendas n ++ "\nTotal:  " ++ show(vendaTotal n)
+
+----------------------------------------------------------------------------------
+-- Lista 2 
+
+-- Ex1
+maior :: Int -> Int -> Int
+maior x y 
+  | (x > y) = x
+  | otherwise = y
+
+-- Ex2
+maiorVenda :: Int -> Int
+maiorVenda n
+   | (n == 0) = vendas 0
+   | otherwise = maior(vendas n) (maiorVenda(n-1))
+
+-- Ex3
+maxVenda :: Int -> Int
+maxVenda n
+  | (n==0) = vendas 0
+  | (maiorVenda n == vendas n) = n
+  | otherwise = maxVenda(n-1)
+
+-- Ex4
+zeroVendas :: Int -> Int
+zeroVendas n
+   | (vendas n == 0) = n
+   | otherwise = -1
+
+-- Ex5
+vendasIgual :: Int -> Int -> Int
+vendasIgual s n
+   | (vendas n == s) = n
+   | otherwise = vendasIgual s(n-1) 
+  
+ -- Ex6 
+ zeroVendas2 :: Int -> Int
+ zeroVendas2 n
+   | (vendasIgual 0 (n)) = n
+   | otherwise = -1
+
+
