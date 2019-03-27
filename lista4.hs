d738c2c7e571f82d6a157eaa6bbeff14302b6204
Geovana Silveira <geovanasilvadasilveira1@gmail.com>
2019-03-27 18:06:47 -0300

    Add files via upload

---
+++ lista4.hs
@@ -0,0 +1,32 @@
+-- Exercicios sobre Listas
+
+--Exemplo Aula
+somaLista :: [Int] -> Int
+somaLista [] = 0
+somaLista (a:x) = a + somaLista x
+
+-- Lista 4
+dobraLista :: [Int] -> [Int]
+dobraLista [] = []
+dobraLista (a:x) = 2 * a : dobraLista x
+
+tamanho :: [Int] -> Int
+tamanho [] = 0
+tamanho (a:x) = 1 + tamanho x
+
+produtoLista :: [Int] -> Int
+produtoLista [] = 1
+produtoLista [a] = a
+produtoLista (a:x) = a * produtoLista x
+
+-- andLista :: [Bool] -> [Bool]
+-- andLista [] = [False]
+-- andLista (a:x) = a && (andLista x)
+
+concatLista :: [[Int]] -> [Int]
+concatLista [[]] = []
+concatLista (a:x) = a ++ (concatLista x)
+
+inverteLista :: [Int] -> [Int]
+inverteLista [] = []
+inverteLista (a:x) = x : inverteLista a
