cd9874c3d0ef1ee65f809493413ee37104f420f9
GeovanaS <geovanasilvadasilveira1@gmail.com>
2019-04-03 01:01:43 +0000

    exercicios do slide

--- aula5.hs
+++ aula5.hs
@@ -15,4 +15,25 @@ maiorEmenor1 :: [Int] -> (Int,Int)
 maiorEmenor1 l = let l2 = iSort l   
                   in(head l2, head(reverse l2) )
 
- 
+-- Retira numeros repetidos da lista
+ins1 :: Int -> [Int] -> [Int]
+ins1 n [] = [n]
+ins1 n (x:xS)
+      | (x==n) = n:xS
+      | (n < x) = n:(x:xS)
+      | otherwise = x:ins1 n xS
+
+iSort1 :: [Int] -> [Int]
+iSort1 [] = []
+iSort1(x:xS) = ins1 x (iSort1 xS)
+
+-- Ordena de forma decrescente
+ins2 :: Int -> [Int] -> [Int]
+ins2 n [] = [n]
+ins2 n (x:xS)
+      | (n > x) = n:(x:xS)
+      | otherwise = x:ins2 n xS
+
+iSort2 :: [Int] -> [Int]
+iSort2 [] = []
+iSort2(x:xS) = ins2 x (iSort2 xS)
