b8919a2296536730ee479c62e187d7e06c6941f1
GeovanaS <geovanasilvadasilveira1@gmail.com>
2019-04-03 00:08:10 +0000

    lista 5 de PF

--- lista5.hs
+++ lista5.hs
@@ -1,21 +1,41 @@
 membro :: [Int] -> Int -> Bool
+membro [] n = False
 membro(x:xS) n 
-       | (x==n) = True
-       | otherwise = False
+       | (x == n) = True
+       | otherwise = membro xS n
 
 membroNum :: [Int] -> Int -> Int
+membroNum [] n = 0
 membroNum(x:xS) n
      | (x == n) = 1 + membroNum xS n
-     | otherwise = 0
+     | otherwise = membroNum xS n
 
 membro1 :: [Int] -> Int -> Bool
 membro1 (x:xS) n
-       | (membroNum xS n > 0) = True
-       | otherwise = False 
+       | membroNum (x:xS) n == 0 = False
+       | otherwise = True
 
-unico :: [Int] -> [Int]
-unico l = unico2 l l
+removeLista :: [Int] -> Int -> [Int]
+removeLista [] n = []
+removeLista (x:xS) n
+      | n == x = removeLista xS n
+      | otherwise = x:removeLista xS n
 
--- unico2 :: [Int] -> [Int]
---unico2 (x:xS) 
-     
+unico :: [Int] -> [Int]      
+unico [] = []
+unico (x:xS)
+     | (membroNum(x:xS) x == 1) = x : unico xS
+     | (membroNum(x:xS) x > 1) = unico(removeLista(x:xS) x)
+
+membro2 :: [Int] -> Int -> Bool
+membro2 x n = membro2(iSort x) n
+
+ins :: Int -> [Int] -> [Int]
+ins n [] = [n]
+ins n (x:xS)
+    | n >= x = x: ins n xS
+    | otherwise = [n,x] ++ xS
+
+iSort :: [Int] -> [Int]
+iSort [] = []
+iSort(x:xS) = ins x (iSort xS)
