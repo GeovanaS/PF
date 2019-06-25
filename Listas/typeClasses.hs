f78113511f9549a2ddff917dd227d41e11a13392
Geovana Silveira <geovanasilvadasilveira1@gmail.com>
2019-06-25 17:19:58 -0300

    Add files via upload

--- Listas/typeClasses.hs
+++ Listas/typeClasses.hs
@@ -1,6 +1,13 @@
 -- Aula 25/06
 
+-- 26/06: Duvida listas/trabalho
+-- 02/07: Duvida listas/trabalho
+-- 03/07: Revisão/Duvidas para a prova
+
 -- Prova 2: 10/07
 -- Apresentação trabalho: 09/07
+
+
+-- Prova 2 com foco em tipos algebricos
 
 
